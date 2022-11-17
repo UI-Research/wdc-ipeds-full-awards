d4582408de959cf864003c0e168f2464295fad59
Claytonseraphin <clayton0687@gmail.com>
2022-11-16 20:46:11 -0500

    Fixed production files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 const iconv = require("iconv-lite");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('./'));
 app.use(express.json());
 
 app.get("/", (req, res) => {
